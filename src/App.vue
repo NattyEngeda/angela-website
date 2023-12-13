299975b95022bd5f676f8451f6d57d3a5a1f96c0
nattyengeda <nattynengeda@gmail.com>
2023-12-13 16:29:49 +0300

    Primary Commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Header from "./layout/Header.vue";
+</script>
+
+<template>
+  <main>
+    <Header />
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/layout/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header
+    class="w-full h-20 bg-whiteflex flex-row gap-10 items-center justify-start boor"
+  >
+    <div>
+      <img class="w-20 h-20" src="../assets/logo/logo.jpg" alt="Logo" />
+    </div>
+  </header>
+</template>
+
+
+<script lang="ts">
+export default {
+  name: "Header",
+};
+</script>
+
+
+
+
+<style lang="scss" scoped>
+</style>
