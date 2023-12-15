d0264396fd8fb59a7ffa289e4d5b4fbaf504f9df
nattyengeda <nattynengeda@gmail.com>
2023-12-15 15:07:39 +0300

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import Header from "./layout/Header.vue";
+  import Header from "./layout/Header.vue";
+  import Home from "./pages/Home.vue";
+  import Footer from "./layout/Footer.vue";
 </script>
 
 <template>
   <main>
     <Header />
-    <h1>Header</h1>
-  </main>
+    <Home/>  
+    <Footer/>  
+</main>
 </template>
 
 <style scoped>
---
+++ src/pages/Home.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import Section1 from '../components/Section1.vue';
+import Section2 from '../components/Section2.vue';
+import SectionContact from '../components/SectionContact.vue';
+
+export default {
+    name: "Home",
+    components: { Section1, Section2, SectionContact }
+}
+</script>
+
+
+<template>
+  <div class="w-full min-h-screen flex flex-col gap-5">
+    <Section1/>
+    <Section2/>
+    <SectionContact/>
+  </div>
+</template>
+
+
+
+
+
+<style lang="scss" scoped>
+
+</style>
